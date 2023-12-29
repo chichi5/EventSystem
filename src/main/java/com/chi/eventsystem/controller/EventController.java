b934f68adeda88b983117c54d794def2fc74a9fa
chichi5 <cchichic757@gmail.com>
2023-12-29 15:56:47 +0800

    eventCount 加入參數

--- src/main/java/com/chi/eventsystem/controller/EventController.java
+++ src/main/java/com/chi/eventsystem/controller/EventController.java
@@ -3,7 +3,9 @@ package com.chi.eventsystem.controller;
 import com.chi.eventsystem.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EventController {
@@ -11,9 +13,12 @@ public class EventController {
     EventService eventService;
 
 
-    // http://localhost:8064/event/cont  (參數: 1 2 3 4 5)
+    // http://localhost:8064/event/cont?cate=2  (參數: 1 2 3 4 5)
     @GetMapping("/event/cont")
-    public String eventCount(){
-       return "";
+    public String eventCount(@RequestParam("cate") int type, Model model){
+        // @RequestParam("cate")轉換為type
+       int count = eventService.getEventCount(type);
+       model.addAttribute("cnt",count);
+       return "event_count";
     }
 }
